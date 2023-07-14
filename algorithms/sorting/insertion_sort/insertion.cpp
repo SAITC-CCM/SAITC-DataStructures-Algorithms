248b346c1c13e30939f47842726746b2f90ecce3
Ernesto Miranda <ernomitrovic@zohomail.com>
2023-07-14 17:58:12 -0600

    Insertion sort in cpp

---
+++ algorithms/sorting/insertion_sort/insertion.cpp
@@ -0,0 +1,34 @@
+/**
+ * @brief Quick implementation of insertion sort.
+ * @tparam T Type of data to be sorted.
+ * @param data Array of data to be sorted.
+ * @param size Size of the array.
+ */
+template <typename T>
+void insertion_sort(T *data, int size)
+{
+    // We assume that the first element is already sorted
+    for (int i = 1; i < size; i++)
+    {
+        T key = data[i];
+        int j = i - 1;
+        while (j >= 0 && data[j] > key)
+        {
+            data[j + 1] = data[j];
+            j--;
+        }
+        data[j + 1] = key;
+    }
+}
+#include <iostream>
+int main()
+{
+    int data[] = {5, 2, 4, 6, 1, 3};
+    insertion_sort(data, 6);
+    for (int i = 0; i < 6; i++)
+    {
+        std::cout << data[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
