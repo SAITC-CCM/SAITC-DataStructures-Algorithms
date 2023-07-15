3d0ba8ab751e2366fa3e0703716dcd51632d1de4
Ernesto Miranda <ernomitrovic@zohomail.com>
2023-07-15 11:50:58 -0600

    Insertion sort in javascript

---
+++ algorithms/sorting/insertion_sort/insertion.js
@@ -0,0 +1,20 @@
+/**
+ * Insertion sort
+ * @param {Array} arr
+ * @returns {Array}
+ */
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+
+    return arr;
+}
